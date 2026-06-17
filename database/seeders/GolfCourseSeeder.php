e198bb645472d1149570b8534c11d3c9614978b9
Shinobu Abe <shinoabe@gmail.com>
2026-06-17 09:44:28 +0900

    データベース文字コード対応

--- database/seeders/GolfCourseSeeder.php
+++ database/seeders/GolfCourseSeeder.php
@@ -9,7 +9,7 @@ class GolfCourseSeeder extends Seeder
 {
     public function run(): void
     {
-        // 開発・動作確認用のダミーデータを 50 件投入
-        GolfCourse::factory()->count(50)->create();
+        // 開発・動作確認用のダミーデータを 100 件投入
+        GolfCourse::factory()->count(100)->create();
     }
 }
